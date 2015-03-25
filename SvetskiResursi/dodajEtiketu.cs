48c43ac359cd3f9b9f911db43f28824a87445bbd
ImolaFodor <fodorimola@gmail.com>
2015-03-25 14:31:54 +0100

    Komitovan dodajEtiketu

---
+++ SvetskiResursi/dodajEtiketu.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SvetskiResursi
+{
+    public partial class dodajEtiketu : Form
+    {
+        public dodajEtiketu()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //if (colorDialog2.ShowDialog() == DialogResult.OK)
+            //{
+                //this.BackColor = colorDialog2.Color;
+            //}
+        }
+    }
+}
