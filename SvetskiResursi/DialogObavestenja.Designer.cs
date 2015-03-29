3886f4c20d0d6d6959ec6175fdd6210a8619a2ac
Fireblume <violeta.vns@gmail.com>
2015-03-29 10:01:13 +0200

    Provera za etiketu, da li su uneta obavezna polja

--- SvetskiResursi/DialogObavestenja.Designer.cs
+++ SvetskiResursi/DialogObavestenja.Designer.cs
@@ -35,17 +35,20 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(88, 23);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(77, 31);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(156, 13);
+            this.label1.Size = new System.Drawing.Size(354, 29);
             this.label1.TabIndex = 0;
             this.label1.Text = "Obavezno polje nije popunjeno!";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(118, 96);
+            this.button1.Location = new System.Drawing.Point(177, 133);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(112, 32);
             this.button1.TabIndex = 1;
             this.button1.Text = "Ok";
             this.button1.UseVisualStyleBackColor = true;
@@ -53,12 +56,15 @@
             // 
             // DialogObavestenja
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(336, 131);
+            this.ClientSize = new System.Drawing.Size(504, 181);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "DialogObavestenja";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "DialogObavestenja";
             this.ResumeLayout(false);
             this.PerformLayout();
